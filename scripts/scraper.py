98cbf067a1f75ad7f650e665e102fccb5400d0fa
Young Kim <young-k@protonmail.com>
2018-04-17 18:07:41 -0400

    updated reqs with bs4; updated scraper to scrape delta comments

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -1,10 +1,11 @@
+import json
+import lxml
 import praw
 import requests
 from bs4 import BeautifulSoup
-import lxml
 
 def write_sample_to_file(num_posts, num_comments):
-  f = open("data.txt", "w+")
+  f = open("testing.txt", "w+")
 
   reddit = praw.Reddit(client_id='wQaHdUWqKKgaLQ',
                        client_secret='mIDBYtZGLe_Pve7RiGcw2VWc-y4',
@@ -14,28 +15,58 @@ def write_sample_to_file(num_posts, num_comments):
   for submission in reddit.subreddit('changemyview').top(limit=num_posts):
     subs.append(submission)
 
+  ret = []
   for submission in subs:
-    print(submission.title.encode('utf8'))
-    f.write(submission.title.encode('utf8'))
-    f.write("\n")
-    f.write("=======================================")
-    f.write("\n\n")
+    delta_detection = submission.author is not None
+
+    curr = {}
+    curr["body"] = ""
+    soup = BeautifulSoup(submission.selftext_html, 'lxml')
+    for paragraph in soup.find_all("p"):
+      curr["body"] += paragraph.text
+
+    if delta_detection: 
+      curr["author"] = submission.author.name
+    else:
+      curr["author"] = "[deleted]"
+
+    curr["title"] = submission.title.encode('utf8')
+    curr["url"] = submission.url
+    print(curr["title"])
 
     submission.comment_sort = 'top'
     top_comments = submission.comments.list()
-    real_comments = [comment for comment in top_comments if isinstance(comment, praw.models.Comment)]
+
+    real_comments = [comment for comment in top_comments \
+        if isinstance(comment, praw.models.Comment)]
+
+    real_comments = [comment for comment in real_comments \
+        if comment.author is not None]
 
     real_comments.sort(key=lambda c: c.score, reverse=True)
 
+    curr["top_comments"] = []
+    curr["delta_comments"] = []
     for comment in real_comments[:num_comments]:
-      f.write("\tScore: " + str(comment.score))
-      f.write("\n")
-      f.write("\t\t" + comment.body.encode('utf8').replace("\n", " "))
-      f.write("\n\n")
+      curr_comment = {}
+      curr_comment["score"] = comment.score
+      curr_comment["author"] = comment.author.name
+      curr_comment["body"] = comment.body.encode('utf8')
+      curr["top_comments"].append(curr_comment)
 
-    f.write("\n\n")
+      if delta_detection:
+        for reply in comment._replies:
+          if isinstance(reply, praw.models.Comment) and \
+              reply.author is not None:
+            if reply.author.name == curr["author"]:
+              delta_given = "!delta" in reply.body or u'\u2206' in reply.body
+              if delta_given:
+                curr["delta_comments"].append(curr_comment)
 
+    ret.append(curr)
+
+  json.dump(ret, f)
   f.close()
 
 if __name__ == "__main__":
-  write_sample_to_file(5000, 25)
+  write_sample_to_file(5, 25)
