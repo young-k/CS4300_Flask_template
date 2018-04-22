6d7b0aef1c03926c71e5dc96067fa4fc74268610
Yuji Akimoto <ya242@cornell.edu>
2018-04-22 19:11:11 -0400

    Update setup script

--- entailment/models/setup.sh
+++ entailment/models/setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 wget https://s3-us-west-2.amazonaws.com/allennlp/models/decomposable-attention-elmo-2018.02.19.tar.gz
+tar -xzf decomposable-attention-elmo-2018.02.19.tar.gz
 wget https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_options.json
 wget https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5
