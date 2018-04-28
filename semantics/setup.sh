3d35390748aba9c2649c6801c72310e087c04a74
cds <ya242@cornell.edu>
2018-04-28 01:05:45 -0400

    Setup script

--- semantics/setup.sh
+++ semantics/setup.sh
@@ -1,4 +1,4 @@
 wget -P ../data/ http://nlp.stanford.edu/data/glove.840B.300d.zip
-unzip glove.840B.300d.zip
-rm *.zip
+unzip ../data/glove.840B.300d.zip
+rm ../data/*.zip
 curl https://s3.amazonaws.com/senteval/infersent/infersent.allnli.pickle
